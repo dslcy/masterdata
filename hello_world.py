4f6dd5391012398dde975132458f03ab5cf1b9da
ceydadisli <54502365+ceydadisli@users.noreply.github.com>
2023-12-17 17:08:17 +0100

    New lines added

---
+++ hello_world.py
@@ -0,0 +1,27 @@
+variable_name= "hello THIS is My Name CEYDA disli"
+#print(variable_name.title())
+#print(variable_name.lower())
+#print(variable_name.upper())
+
+first_name = "uraz"
+last_name = "disli"
+full_name = first_name + " " + last_name
+message = "Hello," + full_name.title()  + "!"
+#print(message)
+fruits = "Fruit List of the Day:\n\tOrange\n\tBanana\n\tApple"
+print(fruits)
+fruits = "           Fruits List:\n\tLemon\n\tOrange      "
+#print(fruits)
+fruits = (fruits.rstrip())
+#print(fruits)
+fruits = fruits.lstrip() 
+#print(fruits)
+fruits= fruits.strip()
+#print(fruits)
+
+new_line = "Python has a varied List's of Code"
+#print(new_line)
+
+
+calculate = int(1+8*3/2**3)
+print(calculate)
